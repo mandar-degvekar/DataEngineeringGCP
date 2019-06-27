3e7221c0e15356d398908f55f35fd56aac19ab6e
mandar-degvekar <mdegvekar@gmail.com>
2019-06-27 12:07:31 +0530

    Python:6 problem

--- DataStructure/LinearAlgebra/Prob3MatMul.py
+++ DataStructure/LinearAlgebra/Prob3MatMul.py
@@ -1,6 +1,5 @@
+import numpy as np
 X = [[ 5, 1 ,3], [ 1, 1 ,1], [ 1, 2 ,1]]
 Y = [[1], [2], [3]]
-z = [0,0,0]
-for i in Y:
-    for j in range(len(X[0])):
-       print(i*X[j][0])
+res=np.dot(X,Y)
+print(res)
---
+++ DataStructure/LinearAlgebra/Prob4Mul.py
@@ -0,0 +1,5 @@
+import numpy as np
+x = [[12,7,3],[4 ,5,6],[7,8,9]]
+y = [[5,8,1],[6,7,3],[4,5,9]]
+res=np.dot(x,y)
+print(res)
